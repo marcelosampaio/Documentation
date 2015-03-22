4146ce06eab3afb2afc3b5cf45b05a616ed60471
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-03-22 15:03:49 -0300

    Initial Commit

---
+++ Documentation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documentation
+//
+//  Created by Marcelo Sampaio on 3/22/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Documentation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Documentation
+//
+//  Created by Marcelo Sampaio on 3/22/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
