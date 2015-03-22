c6b890f45e97b7e58cde96591f6e0f14232ade58
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-03-22 15:37:10 -0300

    tags for documentation

--- Documentation/ViewController.h
+++ Documentation/ViewController.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+/*!
+ @class ViewController
+ 
+ @brief This is the header file where my super-code is contained.
+ 
+ This file contains the most important method and properties declaration. It's parted by two methods in total, which can be used to perform temperature conversions.
+ 
+ @author Marcelo Sampaio
+ 
+ @copyright (c) 2015 Marcelo Sampaio
+ 
+ @version 1.0.0
+ 
+ */
 @interface ViewController : UIViewController
 
+/*! @brief This property knows my name */
+@property (nonatomic,strong) NSString *myName;
+
+
+-(float)toCelsius:(float)fromFahrenheit;
 
 @end
 
--- Documentation/ViewController.m
+++ Documentation/ViewController.m
@@ -10,18 +10,36 @@
 
 @interface ViewController ()
 
+
+
 @end
 
 @implementation ViewController
+@synthesize myName;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"Initial Commit");
+    NSLog(@"Initial Commit    name=%@",self.myName);
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Conversions
+/*!
+ @brief It converts temperature degrees from Fahrenheit to Celsius scale.
+ 
+ @discussion This method accepts a float value representing the temperature in <b>Fahrenheit</b> scale and it converts to the <b>Celsius</b> scale. 
+ 
+ @code
+    float f = [self toCelcius:50];
+ @endcode
+ 
+ @remark This is a super-easy method.
+ 
+ @param fromFahrenheit The input value representing the degrees in th Fahrenheit scale.
+ 
+ @return float The degree in the Celsius scale.
+ */
+-(float)toCelsius:(float)fromFahrenheit {
+    return 0.0f;
 }
-
 @end
